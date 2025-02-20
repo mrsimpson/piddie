4ac10438bdc3b67f8078db4cc782a366ab094ee9
Oliver Jägle <github@beimir.net>
2025-02-20 18:40:38 +0100

    chore: compile

--- packages/project-management/src/internal/dexie-implementation.ts
+++ packages/project-management/src/internal/dexie-implementation.ts
@@ -1,4 +1,4 @@
-import Dexie, { Table } from "dexie";
+import Dexie, { type Table } from "dexie";
 import type { Project, ProjectManager } from "../types";
 import type { ChatManager } from "@piddie/chat-context";
 import { generateProjectId } from "../utils/generate-project-id";
