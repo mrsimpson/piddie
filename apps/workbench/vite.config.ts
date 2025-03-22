7be9d23f7ed6171bc83177a1710ee828d9f57110
Oliver Jägle <github@beimir.net>
2025-03-22 09:24:28 +0100

    WIP: fix build

--- apps/workbench/vite.config.ts
+++ apps/workbench/vite.config.ts
@@ -3,8 +3,16 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueDevTools from "vite-plugin-vue-devtools";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import type { PackageJson } from "type-fest";
+import pkg from "./package.json" with { type: "json" };
 
 const iconsPath = "node_modules/@shoelace-style/shoelace/dist/assets/icons";
+
+// Get all @piddie workspace dependencies
+const dependencies = (pkg as PackageJson).dependencies ?? {};
+const workspaceDeps = Object.keys(dependencies).filter(
+  (dep) => dep.startsWith("@piddie/") && dependencies[dep] === "workspace:*"
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,29 +36,34 @@ export default defineConfig({
     VueDevTools()
   ],
   resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "@piddie/project-management": fileURLToPath(
-        new URL("../project-management/src/index.ts", import.meta.url)
-      ),
-      "@piddie/files-management": fileURLToPath(
-        new URL("../files-management/src/index.ts", import.meta.url)
-      ),
-      "@piddie/shared-types": fileURLToPath(
-        new URL("../shared-types/src/index.ts", import.meta.url)
-      )
-    }
-  },
-  optimizeDeps: {
-    include: [
-      "@piddie/project-management",
-      "@piddie/files-management",
-      "@piddie/shared-types"
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url))
+      },
+      {
+        find: /\/assets\/icons\/(.+)/,
+        replacement: `${iconsPath}/$1`
+      },
+      // Dynamically generate aliases for workspace packages
+      ...workspaceDeps.map((dep) => ({
+        find: dep,
+        replacement: fileURLToPath(
+          new URL(
+            `../../packages/${dep.replace("@piddie/", "")}/dist/index.js`,
+            import.meta.url
+          )
+        )
+      }))
     ]
   },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/, /packages/]
+  optimizeDeps: {
+    exclude: workspaceDeps
+  },
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin"
     }
   }
 });
